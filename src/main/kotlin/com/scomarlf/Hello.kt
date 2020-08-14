357998eb1d461a36a0c2184a83a7e45c8e6a43bd
Scomarlf <scomarlf@outlook.com>
2020-08-14 20:16:30 +0800

    project created

---
+++ src/main/kotlin/com/scomarlf/Hello.kt
@@ -0,0 +1,6 @@
+package com.scomarlf
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/scomarlf/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.scomarlf
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
